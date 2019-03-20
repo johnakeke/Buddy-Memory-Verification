efb19a78cec253bc08980c85de2fab6a5d8f1a02
David Sbnan <johnakekejiangke@gmail.com>
2019-03-20 16:33:36 +0800

    Finish Proofs

--- Verification for Buddy Memory Allocation/Memory_Allocation_Security.thy
+++ Verification for Buddy Memory Allocation/Memory_Allocation_Security.thy
@@ -59,10 +59,10 @@ definition alloc_memory :: "Sys_Config \<Rightarrow> State \<Rightarrow> Pool \<
 definition free_memory :: "Sys_Config \<Rightarrow> State \<Rightarrow> Pool \<Rightarrow> Block \<Rightarrow> State"
   where "free_memory sc s po b \<equiv>
            let partmem = get_partmem_byid s (current s) in
-             (if get_partconf_byid sc (current s) \<noteq> None \<and> partmem \<noteq> None \<and> snd (L b) \<in> the partmem then
+             (if get_partconf_byid sc (current s) \<noteq> None \<and> partmem \<noteq> None then
                  (let re = free (zerolevelblocks po) b (memids s) in
                       (if snd (snd re) = True then
-                          s\<lparr>partitionmem := (partitionmem s)(current s := Some (the partmem - {snd (L b)})),
+                          s\<lparr>partitionmem := (partitionmem s)(current s := Some (the partmem - {snd (L b)}  )),
                             mempools := (mempools s - {po}) \<union> {po\<lparr>zerolevelblocks := fst re\<rparr>},
                             memids := fst (snd re)\<rparr>
                        else s))
@@ -91,14 +91,8 @@ definition state_witness :: State
 
 consts s0t :: State
 specification(s0t)
-  current_part: "(partconf sysconf) (current s0t) \<noteq> None"
-  part_mem_diff: "\<forall>p1 p2. p1 \<noteq> p2 \<and> get_partconf_byid sysconf p1 \<noteq> None \<and> get_partconf_byid sysconf p2 \<noteq> None \<and>
-                          get_partmem_byid s0t p1 \<noteq> None \<and> get_partmem_byid s0t p2 \<noteq> None \<longrightarrow>
-                          the (get_partmem_byid s0t p1) \<inter> the (get_partmem_byid s0t p2) = {}"
-  mem_name_diff: "\<forall>m1 m2. m1 \<noteq> m2 \<and> m1 \<in> mempools s0t \<and> m1 \<in> mempools s0t \<longrightarrow>
-                          pname m1 \<noteq> pname m2"
-  mem_ids_diff: "\<forall>m. m \<in> mempools s0t \<longrightarrow> diff_ids_valid (zerolevelblocks m)"
-  sorry
+  s0t_init: "s0t = state_witness"
+  by simp
 
 primrec event_enabled :: "State \<Rightarrow> Event \<Rightarrow> bool"
   where "event_enabled s (hyperc h) = (is_a_partition sysconf (current s))" |
@@ -132,7 +126,7 @@ lemma nintf_neq: "u \<setminus>\<leadsto>  v \<Longrightarrow> u \<noteq> v"  by
 lemma nintf_reflx: "interference1 u u" by auto
 
 definition vpeq_part :: "State \<Rightarrow> partition_id \<Rightarrow> State \<Rightarrow> bool"
-  where "vpeq_part s d t \<equiv> (\<exists>x. x \<in> the ((partitionmem s) d) \<and> x \<in> the ((partitionmem t) d))"
+  where "vpeq_part s d t \<equiv> (partitionmem s) d = (partitionmem t) d"
 
 definition vpeq_sched :: "State \<Rightarrow> domain_id \<Rightarrow> State \<Rightarrow> bool"
   where "vpeq_sched s d t \<equiv> current s = current t"
@@ -211,14 +205,6 @@ interpretation SM_enabled
         SM_enabled.intro[of domain_of_event "scheduler sysconf" vpeq1 interference1 s0t exec_event] by blast
 
 subsection \<open>Some lemmas of security proofs\<close>
-
-lemma alloc_memory_chg_partstate:
-  "\<lbrakk>is_a_partition sysconf (current s);
-  s' = alloc_memory sysconf s po lv\<rbrakk>
-  \<Longrightarrow> (partitionmem s') (current s) = Some (the ((partitionmem s) (current s)) \<union> (snd (snd (snd (alloc (zerolevelblocks po) lv (memids s))))))"
-  unfolding alloc_memory_def get_partconf_byid_def get_partmem_byid_def Let_def
-  apply auto
-  sorry
 
 lemma sche_imp_not_part:
   "is_a_scheduler sysconf d \<Longrightarrow> \<not> (is_a_partition sysconf d)"      
@@ -371,166 +357,5 @@ theorem local_respect:local_respect
     }
     then show ?thesis using local_respect_all_evt by blast
   qed
-
-subsection \<open>proving "allocate memory" satisfying the "step consistent" property\<close>
-lemma alloc_memory_presrv_wk_stp_cons:
-      assumes p1:"is_a_partition sysconf (current s)"
-        and   p2:"reachable0 s \<and> reachable0 t"
-        and   p3:"s \<sim> d \<sim> t"
-        and   p4:"s \<sim> (scheduler sysconf) \<sim> t"
-        and   p5:"(current s) \<leadsto> d"
-        and   p6:"s \<sim> (current s) \<sim> t"
-        and   p7:"s' = alloc_memory sysconf s po lv"
-        and   p8:"t' = alloc_memory sysconf t po lv"
-      shows   "s' \<sim> d \<sim> t'"
-proof(cases "is_a_scheduler sysconf d")
-  assume a0:"is_a_scheduler sysconf d"
-  show ?thesis by (smt a0 interference1_def p1 p5 sche_imp_not_part)
-next
-  assume a1:"\<not> is_a_scheduler sysconf d"
-  show ?thesis
-  proof(cases "is_a_partition sysconf d")
-    assume b0:"is_a_partition sysconf d"
-    show ?thesis
-    proof(cases "current s = d")
-      assume c0:"current s = d"
-      have d0:"vpeq_part s' d t'"
-      proof-
-        {
-          have e1:"partitionmem s' d = partitionmem t' d"
-          proof-
-            {
-              from p3 b0 have f1:"partitionmem s d = partitionmem t d" 
-                using a1 by fastforce
-              have "partitionmem s' d = partitionmem t' d" sorry
-            }
-            then show ?thesis by auto
-          qed
-          then have "vpeq_part s' d t'" using vpeq_part_def by auto
-        }
-        then show ?thesis by auto
-      qed
-      then show ?thesis using a1 b0 c0 by fastforce
-    next
-      assume c1:"current s \<noteq> d"
-      have d1:"vpeq_part s' d t'"
-      proof-
-        {
-          have e1:"partitionmem s' d = partitionmem t' d"
-            using alloc_memory_notchg_partstate
-            by (meson c1 interference1_def p1 p5 sche_imp_not_part)
-          then have "vpeq_part s' d t'" using vpeq_part_def by auto
-        }
-        then show ?thesis by auto
-      qed
-      then show ?thesis using a1 b0 c1 by auto
-    qed
-  next
-    assume b1:"\<not> is_a_partition sysconf d"
-    show ?thesis using a1 b1 by auto
-  qed
-qed
-
-lemma alloc_memory_presrv_wk_stp_cons_e: "weak_step_consistent_e (hyperc (Buddy_Memory_Alloc po lv))"
-  using alloc_memory_presrv_wk_stp_cons weak_step_consistent_e_def exec_event_def mem_Collect_eq
-        non_interference_def singletonD sched_vpeq
-  sorry
- (* by (smt Event.case(1) Hypercall.case(1) domain_of_event.simps(1) event_enabled.simps(1) option.sel prod.simps(2))*)
-
-subsection \<open>proving "free memory" satisfying the "step consistent" property\<close>
-lemma free_memory_presrv_wk_stp_cons_e: "weak_step_consistent_e (hyperc (Buddy_Memory_Free po b))"
-  sorry
-
-subsection \<open>proving "schedule" satisfying the "step consistent" property\<close>
-lemma schedule_presrv_wk_stp_cons:
-    assumes p1:"reachable0 s \<and> reachable0 t"
-      and   p3:"s \<sim> d \<sim> t"
-      and   p5:"(scheduler sysconf) \<leadsto> d"
-      and   p6:"s \<sim> (scheduler sysconf) \<sim> t"
-      and   p7:"s' \<in> schedule sysconf s"
-      and   p8:"t' \<in> schedule sysconf t"
-    shows   "s' \<sim> d \<sim> t'"
-proof(cases "is_a_scheduler sysconf d")
-  assume a0: "is_a_scheduler sysconf d"
-  from p7 p8 have "current s' = current t'" unfolding schedule_def by simp
-  with a0 show ?thesis using a0 p3 p7 p8 schedule_def by auto 
-next
-  assume a1: "\<not> is_a_scheduler sysconf d"
-  show ?thesis
-  proof(cases "is_a_partition sysconf d")
-    assume b0: "is_a_partition sysconf d"
-    with p3 have b00: "vpeq_part s d t"
-      unfolding vpeq1_def by (metis a1 is_a_scheduler_def)
-    have b1: "vpeq_part s' d t'"
-    proof -
-      have c1:"partitionmem s' d = partitionmem t' d"
-      proof - 
-        {
-          from p3 b0 have f1:"partitionmem s d = partitionmem t d" 
-            using a1 by fastforce
-          from p7 have f2:"partitionmem s d = partitionmem s' d"
-            by (simp add: schedule_def)
-          from p8 have f3:"partitionmem t d = partitionmem t' d"
-            by (simp add: schedule_def)
-          with f1 f2 have "partitionmem s' d = partitionmem t' d" by auto
-        }
-        then show ?thesis by auto
-      qed
-      then have "vpeq_part s' d t'" using vpeq_part_def by simp
-      then show ?thesis by auto
-    qed
-    then show ?thesis using a1 b0 by auto
-  next
-    assume b1: "\<not> is_a_partition sysconf d"
-    show ?thesis using p3 p7 p8 schedule_def a1 b1 by auto         
-  qed
-qed
-
-lemma schedule_presrv_wk_stp_cons_e: "weak_step_consistent_e (sys Schedule)"
-  using schedule_presrv_wk_stp_cons weak_step_consistent_e_def exec_event_def mem_Collect_eq
-        non_interference1_def non_interference_def singletonD sched_vpeq
-  by (smt Event.case(2) System_Event.case(1) domain_of_event.simps(2) event_enabled.simps(2)
-          option.sel prod.simps(2) vpeq1_def vpeq_sched_def)
-
-subsection \<open>proving the "weakly step consistent" property\<close>
-theorem weak_step_consistent:weak_step_consistent
-  proof -
-    {
-      fix e
-      have "weak_step_consistent_e e"
-        apply(induct e)
-        using alloc_memory_presrv_wk_stp_cons_e free_memory_presrv_wk_stp_cons_e
-        apply (rule Hypercall.induct)
-        using schedule_presrv_wk_stp_cons_e
-        by (rule System_Event.induct)
-    }
-    then show ?thesis using weak_step_consistent_all_evt by blast
-  qed
-
-subsection \<open>Security Proofs\<close>
-
-theorem noninfluence_sat: noninfluence
-  using local_respect uc_eq_noninf weak_step_consistent weak_with_step_cons by blast
-
-theorem noninfluence_gen_sat: noninfluence_gen
-  using noninf_eq_noninf_gen noninfluence_sat by blast 
-
-theorem weak_noninfluence_sat: weak_noninfluence
-  using noninf_impl_weak noninfluence_sat by blast
-  
-theorem nonleakage_sat: nonleakage
-  using noninf_impl_nonlk noninfluence_sat by blast
-
-theorem noninterference_r_sat: noninterference_r
-  using noninf_impl_nonintf_r noninfluence_sat by blast
-  
-theorem noninterference_sat: noninterference
-  using noninterference_r_sat nonintf_r_impl_noninterf by blast
-  
-theorem weak_noninterference_r_sat: weak_noninterference_r
-  using noninterference_r_sat nonintf_r_impl_wk_nonintf_r by blast
-  
-theorem weak_noninterference_sat: weak_noninterference
-  using noninterference_sat nonintf_impl_weak by blast
 
 end
